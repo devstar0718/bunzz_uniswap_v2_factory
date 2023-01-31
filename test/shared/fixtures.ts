14fb857c598733f8dd7fd459501d656af43a217c
Alex <devstar0718@gmail.com>
2023-01-30 16:08:39 -0800

    Fixed test script for uniswap v2 factory

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -4,7 +4,7 @@ import { deployContract } from 'ethereum-waffle'
 
 import { expandTo18Decimals } from './utilities'
 
-import ERC20 from '../../build/ERC20.json'
+import ERC20 from '../../build/IERC20.json'
 import UniswapV2Factory from '../../build/UniswapV2Factory.json'
 import UniswapV2Pair from '../../build/UniswapV2Pair.json'
 
